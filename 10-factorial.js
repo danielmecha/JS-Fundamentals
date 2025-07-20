41d984562f5c09398788b572f8fdabd3a613a297
danielmecha <98157403+danielmecha@users.noreply.github.com>
2025-07-20 16:35:18 +0300

    Add factorial countdown of numbers

---
+++ 10-factorial.js
@@ -0,0 +1,11 @@
+const arg = Number(process.argv[2]);
+function factorial(n) {
+  if (isNaN(n)) {
+    return 1;
+  }
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+console.log(factorial(arg));
